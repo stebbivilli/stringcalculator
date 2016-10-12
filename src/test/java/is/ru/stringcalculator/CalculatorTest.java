a8cfb3df92796e3776e82abc8bc2b39a3f221e63
Villi <user1@i-1217c9a8.cs1004cloud.internal>
2016-10-12 22:41:11 +0000

    Added test for checking if \n seperation works.

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -28,5 +28,9 @@ public class CalculatorTest {
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
     }
+	@Test
+   public void testnl(){
+	assertEquals(6, Calculator.add("1\n2,3"));
+    }
 
 }
